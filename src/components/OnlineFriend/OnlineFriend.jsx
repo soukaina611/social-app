23714b79d2d6edfa98a11a78697a0c32804eee1b
Soukaina-sekkat <sk.sekkat@gmail.com>
2023-02-28 17:32:17 +0100

    update onlineFriend picture

--- src/components/OnlineFriend/OnlineFriend.jsx
+++ src/components/OnlineFriend/OnlineFriend.jsx
@@ -6,7 +6,7 @@ const onlineFriend = ({user}) => {
   return (
       <li className="listItem">
               <div className="friendImgContainer">
-                <img src={`${BASE_URL}/images/`+user.profilePicture} alt="" className="imgProfileOnline" />
+                <img src={user.profilePicture ? `${BASE_URL}/images/`+user.profilePicture :`${BASE_URL}/images/person/noAvatar.png` } alt="" className="imgProfileOnline" />
                 <span className="onlineIcon"></span>
               </div>
                 <span className="username">{user.username}</span>
